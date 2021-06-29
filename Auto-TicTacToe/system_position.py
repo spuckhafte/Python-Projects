c467d744548919fac545703064a15fe3ed60345a
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2021-06-29 17:02:46 +0530

    Update system_position.py

--- Auto-TicTacToe/system_position.py
+++ Auto-TicTacToe/system_position.py
@@ -1,6 +1,6 @@
 import random
 
-def system_logic_hard(v, options, xLoc, oLoc, i, locations):
+def system_logic_2hard(v, options, xLoc, oLoc, i, locations):
 	#logics for finding the right position of 'o' for wining if possible
 	if v[1]==v[2]=="o" and v[3] not in options :
 		oLoc = 3
@@ -154,7 +154,7 @@ def system_logic_hard(v, options, xLoc, oLoc, i, locations):
 
 #------------------------------------------------------------------------
 
-def system_logic_medium(v, options, xLoc, oLoc, i, locations):
+def system_logic_2medium(v, options, xLoc, oLoc, i, locations):
 	#logics for finding the right position of 'o' for wining if possible
 	if v[1]==v[2]=="o" and v[3] not in options :
 		oLoc = 3
@@ -365,3 +365,380 @@ def system_logic_easy(v, options, xLoc, oLoc, i, locations):
 		pass
 
 	return oLoc
+
+
+#-----------------
+
+
+def system_logic_1hard(v, options, xLoc, oLoc, i, locations):
+	#logics for finding the right position of 'o' for wining if possible
+	if v[1]==v[2]=="o" and v[3] not in options :
+		oLoc = 3
+	elif v[1]==v[3]=="o" and v[2] not in options :
+		oLoc = 2
+	elif v[2]==v[3]=="o" and v[1] not in options :
+		oLoc = 1
+
+	elif v[4]==v[5]=="o" and v[6] not in options :
+		oLoc = 6
+	elif v[4]==v[6]=="o" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[6]=="o" and v[4] not in options :
+		oLoc = 4
+
+	elif v[7]==v[8]=="o" and v[9] not in options :
+		oLoc = 9
+	elif v[7]==v[9]=="o" and v[8] not in options :
+		oLoc = 8
+	elif v[8]==v[9]=="o" and v[7] not in options :
+		oLoc = 7
+
+	elif v[1]==v[4]=="o" and v[7] not in options :
+		oLoc = 7
+	elif v[1]==v[7]=="o" and v[4] not in options :
+		oLoc = 4
+	elif v[4]==v[7]=="o" and v[1] not in options :
+		oLoc = 1
+
+	elif v[2]==v[5]=="o" and v[8] not in options :
+		oLoc = 8
+	elif v[2]==v[8]=="o" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[8]=="o" and v[2] not in options :
+		oLoc = 2
+
+	elif v[3]==v[6]=="o" and v[9] not in options :
+		oLoc = 9
+	elif v[3]==v[9]=="o" and v[6] not in options :
+		oLoc = 6
+	elif v[6]==v[9]=="o" and v[3] not in options :
+		oLoc = 3
+
+	elif v[1]==v[5]=="o" and v[9] not in options :
+		oLoc = 9
+	elif v[1]==v[9]=="o" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[9]=="o" and v[1] not in options :
+		oLoc = 1
+
+	elif v[3]==v[5]=="o" and v[7] not in options :
+		oLoc = 7
+	elif v[3]==v[7]=="o" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[7]=="o" and v[3] not in options :
+		oLoc = 3
+
+	#logics for continuing the game by breaking the triplets of player if possible
+	elif v[1]==v[2]=="x" and v[3] not in options :
+		oLoc = 3
+	elif v[1]==v[3]=="x" and v[2] not in options :
+		oLoc = 2
+	elif v[2]==v[3]=="x" and v[1] not in options :
+		oLoc = 1
+
+	elif v[4]==v[5]=="x" and v[6] not in options :
+		oLoc = 6
+	elif v[4]==v[6]=="x" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[6]=="x" and v[4] not in options :
+		oLoc = 4
+
+	elif v[7]==v[8]=="x" and v[9] not in options :
+		oLoc = 9
+	elif v[7]==v[9]=="x" and v[8] not in options :
+		oLoc = 8
+	elif v[8]==v[9]=="x" and v[7] not in options :
+		oLoc = 7
+
+	elif v[1]==v[4]=="x" and v[7] not in options :
+		oLoc = 7
+	elif v[1]==v[7]=="x" and v[4] not in options :
+		oLoc = 4
+	elif v[4]==v[7]=="x" and v[1] not in options :
+		oLoc = 1
+
+	elif v[2]==v[5]=="x" and v[8] not in options :
+		oLoc = 8
+	elif v[2]==v[8]=="x" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[8]=="x" and v[2] not in options :
+		oLoc = 2
+
+	elif v[3]==v[6]=="x" and v[9] not in options :
+		oLoc = 9
+	elif v[3]==v[9]=="x" and v[6] not in options :
+		oLoc = 6
+	elif v[6]==v[9]=="x" and v[3] not in options :
+		oLoc = 3
+
+	elif v[1]==v[5]=="x" and v[9] not in options :
+		oLoc = 9
+	elif v[1]==v[9]=="x" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[9]=="x" and v[1] not in options :
+		oLoc = 1
+
+	elif v[3]==v[5]=="x" and v[7] not in options :
+		oLoc = 7
+	elif v[3]==v[7]=="x" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[7]=="x" and v[3] not in options :
+		oLoc = 3
+
+	else:
+		#logics for special moves leading towards tie
+
+		if i==1:
+			oLoc = random.choice([5, 1, 3, 7, 9])
+
+		if i==2:
+			if v[5] == "o":
+				while True:
+					system_case_ = [1,3,7,9]
+					oLoc = random.choice(system_case_)
+					if v[oLoc]=="x":
+						continue
+					else:
+						break
+
+			else:
+				while True:
+					if v[1]=="o" or v[3]=="o" or v[7]=="o" or v[9]=="o":
+
+						if v[1]=="o" or v[9]=="o":
+							oLoc = random.choice([3,7])
+							if v[oLoc]=="x":
+								continue
+							else:
+								break
+
+						if v[3]=="o" or v[7]=="o":
+							oLoc = random.choice([1,9])
+							if v[oLoc]=="x":
+								continue
+							else:
+								break
+
+		if i==3:
+			if v[5] == "o":
+				while True:
+					if v[1]=="o" or v[9] == "o":
+						oLoc = random.choice([3,7])
+						if v[oLoc]=="x":
+								continue
+						else:
+							break
+
+
+					if v[3]=="o" or v[7] == "o":
+						oLoc = random.choice([1,9])
+						if v[oLoc]=="x":
+								continue
+						else:
+							break
+			else:
+				while True:
+					if v[1]==v[3]=="o":
+						oLoc = random.choice([7,9])
+						if v[oLoc]=="x" or v[oLoc] == "o":
+							continue
+						else:
+							break
+					if v[3]==v[9]=="o":
+						oLoc = random.choice([1,7])
+						if v[oLoc]=="x" or v[oLoc] == "o":
+							continue
+						else:
+							break
+					if v[9]==v[7]=="o":
+						oLoc = random.choice([1,3])
+						if v[oLoc]=="x" or v[oLoc] == "o":
+							continue
+						else:
+							break
+					if v[7]==v[1]=="o":
+						oLoc = random.choice([3,9])
+						if v[oLoc]=="x" or v[oLoc] == "o":
+							continue
+						else:
+							break
+
+		
+		#logic for random move as the game is neutral
+		if i!=1 and i!=2 and i!=3:
+			oLoc = random.choice(locations)
+			pass
+
+	#logic for double checking if the position of 'o' overlaps occupied grid
+	if v[oLoc] == "o" or v[oLoc] == "x":
+		oLoc = random.choice(locations)
+	else:
+		pass
+
+	return oLoc
+
+#------------------------------------------------------------------------
+
+def system_logic_1medium(v, options, xLoc, oLoc, i, locations):
+	#logics for finding the right position of 'o' for wining if possible
+	if v[1]==v[2]=="o" and v[3] not in options :
+		oLoc = 3
+	elif v[1]==v[3]=="o" and v[2] not in options :
+		oLoc = 2
+	elif v[2]==v[3]=="o" and v[1] not in options :
+		oLoc = 1
+
+	elif v[4]==v[5]=="o" and v[6] not in options :
+		oLoc = 6
+	elif v[4]==v[6]=="o" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[6]=="o" and v[4] not in options :
+		oLoc = 4
+
+	elif v[7]==v[8]=="o" and v[9] not in options :
+		oLoc = 9
+	elif v[7]==v[9]=="o" and v[8] not in options :
+		oLoc = 8
+	elif v[8]==v[9]=="o" and v[7] not in options :
+		oLoc = 7
+
+	elif v[1]==v[4]=="o" and v[7] not in options :
+		oLoc = 7
+	elif v[1]==v[7]=="o" and v[4] not in options :
+		oLoc = 4
+	elif v[4]==v[7]=="o" and v[1] not in options :
+		oLoc = 1
+
+	elif v[2]==v[5]=="o" and v[8] not in options :
+		oLoc = 8
+	elif v[2]==v[8]=="o" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[8]=="o" and v[2] not in options :
+		oLoc = 2
+
+	elif v[3]==v[6]=="o" and v[9] not in options :
+		oLoc = 9
+	elif v[3]==v[9]=="o" and v[6] not in options :
+		oLoc = 6
+	elif v[6]==v[9]=="o" and v[3] not in options :
+		oLoc = 3
+
+	elif v[1]==v[5]=="o" and v[9] not in options :
+		oLoc = 9
+	elif v[1]==v[9]=="o" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[9]=="o" and v[1] not in options :
+		oLoc = 1
+
+	elif v[3]==v[5]=="o" and v[7] not in options :
+		oLoc = 7
+	elif v[3]==v[7]=="o" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[7]=="o" and v[3] not in options :
+		oLoc = 3
+
+	#logics for continuing the game by breaking the triplets of player if possible
+	elif v[1]==v[2]=="x" and v[3] not in options :
+		oLoc = 3
+	elif v[1]==v[3]=="x" and v[2] not in options :
+		oLoc = 2
+	elif v[2]==v[3]=="x" and v[1] not in options :
+		oLoc = 1
+
+	elif v[4]==v[5]=="x" and v[6] not in options :
+		oLoc = 6
+	elif v[4]==v[6]=="x" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[6]=="x" and v[4] not in options :
+		oLoc = 4
+
+	elif v[7]==v[8]=="x" and v[9] not in options :
+		oLoc = 9
+	elif v[7]==v[9]=="x" and v[8] not in options :
+		oLoc = 8
+	elif v[8]==v[9]=="x" and v[7] not in options :
+		oLoc = 7
+
+	elif v[1]==v[4]=="x" and v[7] not in options :
+		oLoc = 7
+	elif v[1]==v[7]=="x" and v[4] not in options :
+		oLoc = 4
+	elif v[4]==v[7]=="x" and v[1] not in options :
+		oLoc = 1
+
+	elif v[2]==v[5]=="x" and v[8] not in options :
+		oLoc = 8
+	elif v[2]==v[8]=="x" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[8]=="x" and v[2] not in options :
+		oLoc = 2
+
+	elif v[3]==v[6]=="x" and v[9] not in options :
+		oLoc = 9
+	elif v[3]==v[9]=="x" and v[6] not in options :
+		oLoc = 6
+	elif v[6]==v[9]=="x" and v[3] not in options :
+		oLoc = 3
+
+	elif v[1]==v[5]=="x" and v[9] not in options :
+		oLoc = 9
+	elif v[1]==v[9]=="x" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[9]=="x" and v[1] not in options :
+		oLoc = 1
+
+	elif v[3]==v[5]=="x" and v[7] not in options :
+		oLoc = 7
+	elif v[3]==v[7]=="x" and v[5] not in options :
+		oLoc = 5
+	elif v[5]==v[7]=="x" and v[3] not in options :
+		oLoc = 3
+
+	else:
+		#logics for special moves leading towards tie
+
+		if i==1:
+			oLoc = random.choice([5, 1, 3, 7, 9])
+
+		if i==2:
+			if v[5] == "o":
+				while True:
+					system_case_ = [1,3,7,9]
+					oLoc = random.choice(system_case_)
+					if v[oLoc]=="x":
+						continue
+					else:
+						break
+
+			else:
+				while True:
+					if v[1]=="o" or v[3]=="o" or v[7]=="o" or v[9]=="o":
+
+						if v[1]=="o" or v[9]=="o":
+							oLoc = random.choice([3,7])
+							if v[oLoc]=="x":
+								continue
+							else:
+								break
+
+						if v[3]=="o" or v[7]=="o":
+							oLoc = random.choice([1,9])
+							if v[oLoc]=="x":
+								continue
+							else:
+								break
+		
+		#logic for random move as the game is neutral
+		if i!=1 and i!=2 and i!=3:
+			oLoc = random.choice(locations)
+			pass
+
+	#logic for double checking if the position of 'o' overlaps occupied grid
+	if v[oLoc] == "o" or v[oLoc] == "x":
+		oLoc = random.choice(locations)
+	else:
+		pass
+
+	return oLoc
+
+#------------------------------------------------------------------------
