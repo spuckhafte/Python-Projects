b241048ea445502711bf546834a8b21571aabd7c
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2021-06-29 18:36:17 +0530

    Add files via upload

--- Auto-TicTacToe/Auto-TicTacToe.py
+++ Auto-TicTacToe/Auto-TicTacToe.py
@@ -1,18 +1,19 @@
 import list_package as lp
+import random
 import time
 import system_position
 
 #values of the different positions
 v = {
-    1: " ",
-    2: " ",
-    3: " ", 
-    4: " ", 
-    5: " ",
-    6: " ", 
-    7: " ", 
-    8: " ", 
-    9: " "
+	1: " ",
+	2: " ",
+	3: " ", 
+	4: " ", 
+	5: " ",
+	6: " ", 
+	7: " ", 
+	8: " ", 
+	9: " "
 }
 
 #board showing allocation of different grids to certain number
@@ -21,11 +22,10 @@ board_chart = '''
 |4|5|6|
 |7|8|9|
 '''
-print("TICTACTOE (vs Computer)\n")
+print("TICTACTOE - 60% end up with a Tie (in Hard Mode), can You win ?\n")
 
 time.sleep(0.3)
 level = input("\nChoose level: Easy/Medium/Hard(E/M/H): ").lower()
-play_first = input("First chance -> x/o(player/system): ").lower()
 time.sleep(0.3)
 
 time.sleep(1)
@@ -41,174 +41,84 @@ locations=[1,2,3,4,5,6,7,8,9]
 
 options = ["x", "o"]
 
-oLoc=0
-xLoc=0
+while True:
+	#this increment in 'i' is for checking 'Tie' situation
+	i=i+1
 
-#Player plays first
-if play_first == "x":
-    while True:
-        #this increment in 'i' is for checking 'Tie' situation
-        i=i+1
+	#Player's Turn: ----------------------
+	while True:
+		try:
+			xLoc = int(input('\nx: '))
+			if xLoc in locations:
+				break
+			elif xLoc == 0:
+				print(board_chart)
+			else:
+				print("Invalid Input, Try Again")
+				continue
+		except:
+			print("Invalid Input, Try Again")
+			continue
 
-        #Player's Turn: ----------------------
-        while True:
-            try:
-                xLoc = int(input('\nx: '))
-                if xLoc in locations:
-                    break
-                elif xLoc == 0:
-                    print(board_chart)
-                else:
-                    print("Invalid Input, Try Again")
-                    continue
-            except:
-                print("Invalid Input, Try Again")
-                continue
+	#Assigning x's position (player's positon)
+	v[xLoc] = 'x'
 
-        #Assigning x's position (player's positon)
-        v[xLoc] = 'x'
+	#this location gets removed from that list as it has been used
+	locations.pop(lp.indexOf(locations, xLoc))
 
-        #this location gets removed from that list as it has been used
-        locations.pop(lp.indexOf(locations, xLoc))
+	#board showing x's position
+	board = f'''
+	|{v[1]}|{v[2]}|{v[3]}|
+	|{v[4]}|{v[5]}|{v[6]}|
+	|{v[7]}|{v[8]}|{v[9]}|
+	'''
+	print(board)
 
-        #board showing x's position
-        board = f'''
-        |{v[1]}|{v[2]}|{v[3]}|
-        |{v[4]}|{v[5]}|{v[6]}|
-        |{v[7]}|{v[8]}|{v[9]}|
-        '''
-        print(board)
+	#logic if there is a 'win' for x or a 'tie' or 'the game is still on'
+	if(v[1]==v[2]==v[3]=="x" or v[4]==v[5]==v[6]=="x" or v[7]==v[8]==v[9]=="x" or v[1]==v[4]==v[7]=="x" or v[2]==v[5]==v[8]=="x" or v[3]==v[6]==v[9]=="x" or v[1]==v[5]==v[9]=="x" or v[3]==v[5]==v[7]=="x"):
+		time.sleep(0.5)
+		print("\nPlayer WINS !")
+		time.sleep(2)
+		break
+	elif i==5:
+		time.sleep(0.5)
+		print("\nTIE")
+		time.sleep(2)
+		break
+	else:
+		pass
 
-        #logic if there is a 'win' for x or a 'tie' or 'the game is still on'
-        if(v[1]==v[2]==v[3]=="x" or v[4]==v[5]==v[6]=="x" or v[7]==v[8]==v[9]=="x" or v[1]==v[4]==v[7]=="x" or v[2]==v[5]==v[8]=="x" or v[3]==v[6]==v[9]=="x" or v[1]==v[5]==v[9]=="x" or v[3]==v[5]==v[7]=="x"):
-            time.sleep(0.5)
-            print("\nPlayer WINS !")
-            time.sleep(2)
-            break
-        elif i==5:
-            time.sleep(0.5)
-            print("\nTIE")
-            time.sleep(2)
-            break
-        else:
-            pass
+	#System's Turn: ----------------------
+	oLoc=0
 
-        #System's Turn: ----------------------
-        oLoc=0
+	# Function for getting the most approriate position for system (according to the level selected)
+	if level == "easy" or level == "e":
+		oLoc = system_position.system_logic_easy(v, options, xLoc, oLoc, i, locations)
+	if level == "medium" or level == "m":
+		oLoc = system_position.system_logic_medium(v, options, xLoc, oLoc, i, locations)
+	if level == "hard" or level == "h":
+		oLoc = system_position.system_logic_hard(v, options, xLoc, oLoc, i, locations)
+		
+	#final location of 'o' (system)
+	v[oLoc] = 'o'
 
-        # Function for getting the most approriate position for system (according to the level selected)
-        if level == "easy" or level == "e":
-            oLoc = system_position.system_logic_easy(v, options, xLoc, oLoc, i, locations)
-        if level == "medium" or level == "m":
-            oLoc = system_position.system_logic_2medium(v, options, xLoc, oLoc, i, locations)
-        if level == "hard" or level == "h":
-            oLoc = system_position.system_logic_2hard(v, options, xLoc, oLoc, i, locations)
-            
-        #final location of 'o' (system)
-        v[oLoc] = 'o'
-        locations.pop(lp.indexOf(locations, oLoc))
+	board = f'''
+	|{v[1]}|{v[2]}|{v[3]}|
+	|{v[4]}|{v[5]}|{v[6]}|
+	|{v[7]}|{v[8]}|{v[9]}|
+	'''
 
-        board = f'''
-        |{v[1]}|{v[2]}|{v[3]}|
-        |{v[4]}|{v[5]}|{v[6]}|
-        |{v[7]}|{v[8]}|{v[9]}|
-        '''
+	#interactive text for engaging the player
+	time.sleep(0.5)
+	print('\nComputer is thinking...')
+	time.sleep(1.5)
+	print(board)
 
-        #interactive text for engaging the player
-        time.sleep(0.5)
-        print('\nComputer is thinking...')
-        time.sleep(1.5)
-        print(board)
-
-        #logic to check if there is a 'win' or 'the game is still going on' for system
-        if(v[1]==v[2]==v[3]=="o" or v[4]==v[5]==v[6]=="o" or v[7]==v[8]==v[9]=="o" or v[1]==v[4]==v[7]=="o" or v[2]==v[5]==v[8]=="o" or v[3]==v[6]==v[9]=="o" or v[1]==v[5]==v[9]=="o" or v[3]==v[5]==v[7]=="o"):
-            time.sleep(0.5)
-            print("\nComputer WINS !")
-            time.sleep(2)
-            break
-        else:
-            continue
-
-#System plays first
-if play_first == "o":
-    #System's Turn: ----------------------
-    while True:
-        i=i+1
-
-        oLoc=0
-
-        # Function for getting the most approriate position for system (according to the level selected)
-        if level == "easy" or level == "e":
-            oLoc = system_position.system_logic_easy(v, options, xLoc, oLoc, i, locations)
-        if level == "medium" or level == "m":
-            oLoc = system_position.system_logic_1medium(v, options, xLoc, oLoc, i, locations)
-        if level == "hard" or level == "h":
-            oLoc = system_position.system_logic_1hard(v, options, xLoc, oLoc, i, locations)
-            
-        #final location of 'o' (system)
-        v[oLoc] = 'o'
-        locations.pop(lp.indexOf(locations, oLoc))
-
-        board = f'''
-        |{v[1]}|{v[2]}|{v[3]}|
-        |{v[4]}|{v[5]}|{v[6]}|
-        |{v[7]}|{v[8]}|{v[9]}|
-        '''
-
-        #interactive text for engaging the player
-        time.sleep(0.5)
-        print('\nComputer is thinking...')
-        time.sleep(1.5)
-        print(board)
-
-        #logic to check if there is a 'win' or 'the game is still going on' for system
-        if(v[1]==v[2]==v[3]=="o" or v[4]==v[5]==v[6]=="o" or v[7]==v[8]==v[9]=="o" or v[1]==v[4]==v[7]=="o" or v[2]==v[5]==v[8]=="o" or v[3]==v[6]==v[9]=="o" or v[1]==v[5]==v[9]=="o" or v[3]==v[5]==v[7]=="o"):
-            time.sleep(0.5)
-            print("\nComputer WINS !")
-            time.sleep(2)
-            break
-        elif i==5:
-            time.sleep(0.5)
-            print("\nTIE")
-            time.sleep(2)
-            break
-        else:
-            pass
-
-    #Player's Turn: ----------------------
-        while True:
-            try:
-                xLoc = int(input('\nx: '))
-                if xLoc in locations:
-                    break
-                elif xLoc == 0:
-                    print(board_chart)
-                else:
-                    print("Invalid Input, Try Again")
-                    continue
-            except:
-                print("Invalid Input, Try Again")
-                continue
-
-        #Assigning x's position (player's positon)
-        v[xLoc] = 'x'
-
-        #this location gets removed from that list as it has been used
-        locations.pop(lp.indexOf(locations, xLoc))
-
-        #board showing x's position
-        board = f'''
-        |{v[1]}|{v[2]}|{v[3]}|
-        |{v[4]}|{v[5]}|{v[6]}|
-        |{v[7]}|{v[8]}|{v[9]}|
-        '''
-        print(board)
-
-        #logic if there is a 'win' for x or a 'tie' or 'the game is still on'
-        if(v[1]==v[2]==v[3]=="x" or v[4]==v[5]==v[6]=="x" or v[7]==v[8]==v[9]=="x" or v[1]==v[4]==v[7]=="x" or v[2]==v[5]==v[8]=="x" or v[3]==v[6]==v[9]=="x" or v[1]==v[5]==v[9]=="x" or v[3]==v[5]==v[7]=="x"):
-            time.sleep(0.5)
-            print("\nPlayer WINS !")
-            time.sleep(2)
-            break
-        else:
-            pass
+	#logic to check if there is a 'win' or 'the game is still going on' for system
+	if(v[1]==v[2]==v[3]=="o" or v[4]==v[5]==v[6]=="o" or v[7]==v[8]==v[9]=="o" or v[1]==v[4]==v[7]=="o" or v[2]==v[5]==v[8]=="o" or v[3]==v[6]==v[9]=="o" or v[1]==v[5]==v[9]=="o" or v[3]==v[5]==v[7]=="o"):
+		time.sleep(0.5)
+		print("\nComputer WINS !")
+		time.sleep(2)
+		break
+	else:
+		continue
