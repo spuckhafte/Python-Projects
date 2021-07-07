60b89a7634c254a9efa7933c3bbb7a2ed90d3bbc
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2021-07-07 21:21:01 +0530

    Update system_position.py

--- Auto-TicTacToe-Renewed/system_position.py
+++ Auto-TicTacToe-Renewed/system_position.py
@@ -1,126 +1,45 @@
 import random
 
-#completing winning row
-def win(v, oLoc, xLoc, options, locations):
-	if v[1]==v[2]=="o" and v[3] not in options :
-		oLoc = 3
-	elif v[1]==v[3]=="o" and v[2] not in options :
-		oLoc = 2
-	elif v[2]==v[3]=="o" and v[1] not in options :
-		oLoc = 1
+def win(v, oLoc, options, level, winning_rows):
+	condition = True
+	for row in winning_rows:
+		for element in row:
+			position1 = element
+			for position2 in row:
+				if position1 != position2 and v[position1] == v[position2] == "o":
+					row.remove(position1)
+					row.remove(position2)
 
-	elif v[4]==v[5]=="o" and v[6] not in options :
-		oLoc = 6
-	elif v[4]==v[6]=="o" and v[5] not in options :
-		oLoc = 5
-	elif v[5]==v[6]=="o" and v[4] not in options :
-		oLoc = 4
+					if v[row[0]] not in options:
+						oLoc = row[0]
+						condition = False
+						break
+					else:
+						pass
+				else:
+					continue
 
-	elif v[7]==v[8]=="o" and v[9] not in options :
-		oLoc = 9
-	elif v[7]==v[9]=="o" and v[8] not in options :
-		oLoc = 8
-	elif v[8]==v[9]=="o" and v[7] not in options :
-		oLoc = 7
+	if condition:
+		for row in winning_rows:
+			for element in row:
+				position1 = element
+				for position2 in row:
+					if position1 != position2 and v[position1] == v[position2] == "x":
+						row.remove(position1)
+						row.remove(position2)
 
-	elif v[1]==v[4]=="o" and v[7] not in options :
-		oLoc = 7
-	elif v[1]==v[7]=="o" and v[4] not in options :
-		oLoc = 4
-	elif v[4]==v[7]=="o" and v[1] not in options :
-		oLoc = 1
-
-	elif v[2]==v[5]=="o" and v[8] not in options :
-		oLoc = 8
-	elif v[2]==v[8]=="o" and v[5] not in options :
-		oLoc = 5
-	elif v[5]==v[8]=="o" and v[2] not in options :
-		oLoc = 2
-
-	elif v[3]==v[6]=="o" and v[9] not in options :
-		oLoc = 9
-	elif v[3]==v[9]=="o" and v[6] not in options :
-		oLoc = 6
-	elif v[6]==v[9]=="o" and v[3] not in options :
-		oLoc = 3
-
-	elif v[1]==v[5]=="o" and v[9] not in options :
-		oLoc = 9
-	elif v[1]==v[9]=="o" and v[5] not in options :
-		oLoc = 5
-	elif v[5]==v[9]=="o" and v[1] not in options :
-		oLoc = 1
-
-	elif v[3]==v[5]=="o" and v[7] not in options :
-		oLoc = 7
-	elif v[3]==v[7]=="o" and v[5] not in options :
-		oLoc = 5
-	elif v[5]==v[7]=="o" and v[3] not in options :
-		oLoc = 3
-
-	#logics for continuing the game by breaking the triplets of player if possible
-	elif v[1]==v[2]=="x" and v[3] not in options :
-		oLoc = 3
-	elif v[1]==v[3]=="x" and v[2] not in options :
-		oLoc = 2
-	elif v[2]==v[3]=="x" and v[1] not in options :
-		oLoc = 1
-
-	elif v[4]==v[5]=="x" and v[6] not in options :
-		oLoc = 6
-	elif v[4]==v[6]=="x" and v[5] not in options :
-		oLoc = 5
-	elif v[5]==v[6]=="x" and v[4] not in options :
-		oLoc = 4
-
-	elif v[7]==v[8]=="x" and v[9] not in options :
-		oLoc = 9
-	elif v[7]==v[9]=="x" and v[8] not in options :
-		oLoc = 8
-	elif v[8]==v[9]=="x" and v[7] not in options :
-		oLoc = 7
-
-	elif v[1]==v[4]=="x" and v[7] not in options :
-		oLoc = 7
-	elif v[1]==v[7]=="x" and v[4] not in options :
-		oLoc = 4
-	elif v[4]==v[7]=="x" and v[1] not in options :
-		oLoc = 1
-
-	elif v[2]==v[5]=="x" and v[8] not in options :
-		oLoc = 8
-	elif v[2]==v[8]=="x" and v[5] not in options :
-		oLoc = 5
-	elif v[5]==v[8]=="x" and v[2] not in options :
-		oLoc = 2
-
-	elif v[3]==v[6]=="x" and v[9] not in options :
-		oLoc = 9
-	elif v[3]==v[9]=="x" and v[6] not in options :
-		oLoc = 6
-	elif v[6]==v[9]=="x" and v[3] not in options :
-		oLoc = 3
-
-	elif v[1]==v[5]=="x" and v[9] not in options :
-		oLoc = 9
-	elif v[1]==v[9]=="x" and v[5] not in options :
-		oLoc = 5
-	elif v[5]==v[9]=="x" and v[1] not in options :
-		oLoc = 1
-
-	elif v[3]==v[5]=="x" and v[7] not in options :
-		oLoc = 7
-	elif v[3]==v[7]=="x" and v[5] not in options :
-		oLoc = 5
-	elif v[5]==v[7]=="x" and v[3] not in options :
-		oLoc = 3
-	else:
-		pass
+						if v[row[0]] not in options and level != "e":
+							oLoc = row[0]
+							condition = True
+							break
+						else:
+							pass
+					else:
+						continue
 
 	return oLoc
 
 
-#logic for easy game
 def system_logic_easy(v, options, xLoc, oLoc, i, locations):
 	#logic for random move as the game is neutral
 	oLoc = random.choice(locations)
@@ -134,7 +53,6 @@ def system_logic_easy(v, options, xLoc, oLoc, i, locations):
 
 	return oLoc
 
-#logic for medium game if system starts first
 def system_logic_medium_first(v, options, xLoc, oLoc, i, locations):
 	if i==1:
 		oLoc = random.choice([5, 1, 3, 7, 9])
@@ -174,7 +92,6 @@ def system_logic_medium_first(v, options, xLoc, oLoc, i, locations):
 
 	return oLoc
 
-#logic for medium game if system plays second
 def system_logic_medium_second(v, options, xLoc, oLoc, i, locations):
 	#logics for special moves leading towards tie
 	if i==2:
@@ -199,7 +116,42 @@ def system_logic_medium_second(v, options, xLoc, oLoc, i, locations):
 
 	return oLoc
 
-#logic for hard game if system starts first
+def system_logic_hard_second(v, options, xLoc, oLoc, i, locations):
+	#logics for special moves leading towards tie
+	if i==1:
+		if xLoc == 5:
+			oLoc = random.choice([7,3])
+		else: 
+			oLoc = 5
+
+	if i==2:
+		while True:
+			if v[7] == "o" or v[3] == "o" :
+				_system_case_ = [1,9]
+				oLoc = random.choice(_system_case_)
+				break
+
+			else:
+				system_case_ = [2,8,4,6]
+				oLoc = random.choice(system_case_)
+				if v[oLoc] not in options:
+					break 
+				else:
+					continue
+		
+	#logic for random move as the game is neutral
+	if i!=1 and i!=2:
+		oLoc = random.choice(locations)
+		pass
+
+	#logic for double checking if the position of 'o' overlaps occupied grid
+	if v[oLoc] == "o" or v[oLoc] == "x":
+		oLoc = random.choice(locations)
+	else:
+		pass
+
+	return oLoc
+
 def system_logic_hard_first(v, options, xLoc, oLoc, i, locations):
 	#logics for special moves leading towards tie
 	if i==1:
@@ -280,43 +232,6 @@ def system_logic_hard_first(v, options, xLoc, oLoc, i, locations):
 	#logic for random move as the game is neutral
 	if i!=1 and i!=2 and i!=3:
 		oLoc = random.choice(locations)
-		pass
-
-	return oLoc
-
-#logic for hard game if system plays second
-def system_logic_hard_second(v, options, xLoc, oLoc, i, locations):
-	#logics for special moves leading towards tie
-	if i==1:
-		if xLoc == 5:
-			oLoc = random.choice([7,3])
-		else:
-			oLoc = 5
-
-	if i==2:
-		while True:
-			if v[7] == "o" or v[3] == "o" :
-				_system_case_ = [1,9]
-				oLoc = random.choice(_system_case_)
-				break
-
-			else:
-				system_case_ = [2,8,4,6]
-				oLoc = random.choice(system_case_)
-				if v[oLoc] not in options:
-					break 
-				else:
-					continue
-		
-	#logic for random move as the game is neutral
-	if i!=1 and i!=2:
-		oLoc = random.choice(locations)
-		pass
-
-	#logic for double checking if the position of 'o' overlaps occupied grid
-	if v[oLoc] == "o" or v[oLoc] == "x":
-		oLoc = random.choice(locations)
-	else:
 		pass
 
 	return oLoc
