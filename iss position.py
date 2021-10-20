65aba030479f7dde8b6c99ceda97ebe23fa2c297
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2021-10-20 12:27:44 +0530

    Update iss position.py

--- iss position.py
+++ iss position.py
@@ -1,13 +1,7 @@
-import json
 import requests
 
 while True:
     result = requests.get('http://api.open-notify.org/iss-now.json').json()
     lat = result['iss_position']['latitude']
     long = result['iss_position']['longitude']
-
     print(lat, long)
-    data = {'lat': lat, 'long': long}
-    json_data = json.dumps(data)
-    with open('data.json', 'w') as out:
-        out.write(json_data)
